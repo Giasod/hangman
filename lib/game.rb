97ed9a468781aa2b77b84cd235cd2c7c19e6bd25
AmiranTsiklauri <amiranlevanovich@gmail.com>
2020-07-03 20:07:11 +0300

    Created a special directory and added all the required files

---
+++ lib/game.rb
@@ -0,0 +1,67 @@
+class Game
+  TOTAL_ERRORS_ALLOWED = 7
+
+  def initialize(word)
+    @letters = word.chars
+    @user_guesses = []
+  end
+
+  def errors
+    @user_guesses - normalized_letters
+  end
+
+  def errors_made
+    errors.length
+  end
+
+  def errors_allowed
+    TOTAL_ERRORS_ALLOWED - errors_made
+  end
+
+  def normalize_letter(letter)
+    case letter
+    when "Ё"
+      return "Е"
+    when "Й"
+      return "И"
+    end
+
+    letter
+  end
+
+  def normalized_letters
+    @letters.map {|letter| normalize_letter(letter)}
+  end
+
+  def letters_to_guess
+    result =
+      @letters.map do |letter|
+        letter if @user_guesses.include?(normalize_letter(letter))
+      end
+
+    result
+  end
+
+  def over?
+    won? || lost?
+  end
+
+  def play!(letter)
+    if !over? && !@user_guesses.include?(normalize_letter(letter))
+      @user_guesses << normalize_letter(letter)
+    end
+  end
+
+  def won?
+    (normalized_letters - @user_guesses).empty?
+  end
+
+  def lost?
+    errors_allowed == 0
+  end
+
+  def word
+    @letters.join
+  end
+end
+
